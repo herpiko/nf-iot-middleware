0a94c0ecd930bc68db45ea6f9079b3503934f48e
Herpiko <herpiko@melodiys-MacBook-Pro.local>
2015-10-23 23:12:11 +0700

    Minor fixes

--- index.js
+++ index.js
@@ -4,25 +4,31 @@ var io = require("socket.io").listen(server);
 
 var port = process.env.PORT || 3000;
 server.listen(port);
+console.log("Running on PORT 3000");
 
 // Basic routing
 // ng --> middleware --> end
 app.get("/led1", function(req, res){
-  io.to("end").emit("command", req.query.value);
+  io.to("end").emit("command", {key: "led1", value: req.query.value});
   res.send({success:true});
 });
 
 io.sockets.on("connection", function(socket){
-  console.log("a client connected");
+  // For each connection attempt, ask all the socket client,
+  // whether they are an `end` or a `ng`
+  console.log("a client attempting to connect");
+  io.emit("whoareu");
   
   // Join room as end
   socket.on("join-end", function(){
     socket.join("end");
+    console.log("an `end` client connected");
   })
 
   // Or as angular client
   socket.on("join-ng", function(){
     socket.join("ng");
+    console.log("an `ng` client connected");
   })
 
   // Socket event
